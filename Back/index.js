69f0438f30c716f1947b9fb7ecc3f39a3f3b087d
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-01-23 20:16:25 -0300

    GET de tweets concluído

--- Back/index.js
+++ Back/index.js
@@ -15,15 +15,22 @@ app.post("/sign-up", (req, res) => {
   res.send("OK");
 });
 
+app.get("/sign-up", (req, res) => {
+  res.send(USER_DATA);
+})
+
 app.post("/tweets", (req, res) => {
   const tweet = req.body;
   TWEETS_DATA.push(tweet);
   res.send("OK");
 })
-
-
 app.get("/tweets", (req, res) => {
-  res.send("I was wondering if after all these years");
+  const last_ten = TWEETS_DATA.slice(-10);
+  last_ten.map(tweet => {
+    const current_user = USER_DATA.find(user => user.name === tweet.name);
+    tweet.avatar = current_user.avatar;
+  })
+  res.send(last_ten.reverse());
 });
 
 app.listen(5000);
