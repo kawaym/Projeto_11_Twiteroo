cabb310e807e8167108783d731215545d1e2223a
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-01-24 15:34:22 -0300

    Manejamento de pastas

--- Back/index.js
+++ Back/index.js
@@ -11,25 +11,36 @@ app.use(express.json(), cors());
 
 app.post("/sign-up", (req, res) => {
   const user = req.body;
-  USER_DATA.push(user);
-  res.send("OK");
+  if (!user.username || !user.avatar) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    USER_DATA.push(user);
+    res.status(201).send("OK");
+  }
 });
 
 app.get("/sign-up", (req, res) => {
   res.send(USER_DATA);
-})
+});
 
 app.post("/tweets", (req, res) => {
   const tweet = req.body;
-  TWEETS_DATA.push(tweet);
-  res.send("OK");
-})
+  if (!tweet.tweet) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    TWEETS_DATA.push(tweet);
+    res.status(201).send("OK");
+  }
+});
+
 app.get("/tweets", (req, res) => {
   const last_ten = TWEETS_DATA.slice(-10);
-  last_ten.map(tweet => {
-    const current_user = USER_DATA.find(user => user.name === tweet.name);
+  last_ten.map((tweet) => {
+    const current_user = USER_DATA.find(
+      (user) => user.username === tweet.username
+    );
     tweet.avatar = current_user.avatar;
-  })
+  });
   res.send(last_ten.reverse());
 });
 
